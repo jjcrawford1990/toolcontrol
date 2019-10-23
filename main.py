1c292ce5a28ffbda694c3096a71eb8b714f9477f
Josh <jjcrawford1990@gmail.com>
2019-10-23 11:16:43 +0100

    revised loop to not print if no serial data presented

--- main.py
+++ main.py
@@ -2,27 +2,29 @@ import serial
 import rfid
 import gpiozero
 
-#lock = gpiozero.LED(17) #lock is GPIO GP17, LED class must be used
-#rfid_permission_led = gpiozero.LED(18) #LED to flash if not authenticated
+lock = gpiozero.LED(17) #lock is GPIO GP17, LED class must be used
+rfid_permission_led = gpiozero.LED(18) #LED to flash if not authenticated
 
-#ser = serial.Serial(
-#    port='/dev/serial0',
-#    baudrate=9600,
-#    parity=serial.PARITY_NONE,
-#    stopbits=serial.STOPBITS_ONE,
-#    bytesize=serial.EIGHTBITS,
-#    timeout=1
-#)
+ser = serial.Serial(
+    port='/dev/ttyS0',
+    baudrate=9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
 
-#while 1:
-#    raw_data = ser.readline() #read serial port until a '\n' termination byte is recieved
-raw_data = ['2', '02', '6B', '19', '5D', '1', '\n']
-rfid_message = rfid.RFID(raw_data) #pass raw_data to new instance object 'rfid_message'
-if rfid_message.authentication_level == 1:
-    #lock.on()
-    #lock will unlock for 5 seconds
-    print('AUTHENTICATED')
-else:
-   # rfid_permission_led.blink(0.6, 0.4, 4) #blink on for 0.6seconds, off, for 0.4 seconds, blink 4 times
-    print('No Access')
-
+while True:
+    raw_data = ser.readline() #read serial port until a '\n' termination byte is recieved
+    rfid_message = rfid.RFID(raw_data) #pass raw_data to new instance object 'rfid_message'
+    if rfid_message.authentication_level == 1:
+        #lock.on()
+        #lock will unlock for 5 seconds
+        print(raw_data)
+        print('RFID: ' +str(rfid_message.l_to_st))
+        print('Access Level ' + str(rfid_message.authentication_level) + ' granted!')
+    elif len(raw_data) == 0: #if no data is present(timeout reached), do nothing.
+        pass
+    else:
+        rfid_permission_led.blink(0.6, 0.4, 4) #blink on for 0.6seconds, off, for 0.4 seconds, blink 4 times
+        print('No Access')
